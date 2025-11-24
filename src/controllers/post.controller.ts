9cc803ca84570c3498a6654297c84c8149b9a22d
Rashid Ameer <r.ameer.offical@gmail.com>
2025-11-25 02:02:52 +0500

    refactor(post.controller): reorganize s3 client imports
    Consolidate all S3 client related imports from @aws-sdk/client-s3 into a single import statement for better maintainability and readability.

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,7 +1,6 @@
 import { unlink, createReadStream } from "fs";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { GetObjectCommand } from "@aws-sdk/client-s3";
 
 import s3 from "../config/s3.js";
 import HTTP_CODES from "../constants/httpCodes.js";
